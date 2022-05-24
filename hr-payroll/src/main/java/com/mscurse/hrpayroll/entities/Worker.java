8ad123128ea560f8837c5afb6fd086098e61aebd
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-05-23 23:01:43 -0300

    RestTemplate, problemas no getForObject

---
+++ hr-payroll/src/main/java/com/mscurse/hrpayroll/entities/Worker.java
@@ -0,0 +1,79 @@
+package com.mscurse.hrpayroll.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Worker implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+
+	private Long id;
+	private String name;
+	private Double daily_Income;
+	
+	public Worker() {
+		
+	}
+
+	public Worker(Long id, String name, Double daily_Income) {
+		this.id = id;
+		this.name = name;
+		this.daily_Income = daily_Income;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return daily_Income;
+	}
+
+	public void setDailyIncome(Double daily_Income) {
+		this.daily_Income = daily_Income;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
