21c52132a332da97ba17ef6f2d853fe1660a2575
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-05-23 22:04:53 -0300

    criação do payroll

---
+++ hr-payroll/src/test/java/com/mscurse/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscurse.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
